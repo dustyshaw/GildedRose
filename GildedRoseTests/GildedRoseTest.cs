274c930e3062a88f86dcf21c48b5a57fb9704c46
Dusty Shaw <shustydaw@gmail.com>
2024-10-28 13:55:35 -0600

    Add project files.

---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void foo()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal("foo", Items[0].Name);
+    }
+
+    [Fact]
+    public void QualityDegradesByOne()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 2, Quality = 2 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(1, Items[0].Quality);
+    }
+
+    [Fact]
+    public void QualityDegradesTwiceAsFast()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 2 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+    }
+
+    [Fact]
+    public void QualityDegradesByHalfWithOddNumber()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(3, Items[0].Quality);
+    }
+
+    [Fact]
+    public void QualityDegradesByHalfWithOddNumber2()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 7 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(5, Items[0].Quality);
+    }
+}
